af1a24e2e1aa3aef100c52b8442478d8f6b9feb4
Kseniia <shtangksyusha@gmail.com>
2026-01-24 17:27:07 +0100

    Add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "antd/dist/reset.css";
 import "./globals.css";
 import I18nProvider from "./components/I18nProvider";
@@ -211,6 +212,7 @@ export default function RootLayout({
         </ThemeProvider>
         <BuyMeACoffee />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
