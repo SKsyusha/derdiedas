35ea3e29da9d4be9ee2a82d2ed485b761e243421
Kseniia <shtangksyusha@gmail.com>
2026-03-05 19:04:20 +0100

    Add app banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -241,6 +241,7 @@ export default async function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Der Die Das Trainer" />
+        <meta name="apple-itunes-app" content="app-id=6759857767" />
         <link rel="apple-touch-icon" href="/Assets.xcassets/AppIcon.appiconset/180.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/Assets.xcassets/AppIcon.appiconset/180.png" />
         <link rel="apple-touch-icon" sizes="152x152" href="/Assets.xcassets/AppIcon.appiconset/152.png" />
